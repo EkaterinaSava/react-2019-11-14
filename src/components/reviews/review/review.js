c7d583463786b9f6d6db3c62532130b9b7efa70d
Kate Sava <ekaterinasava@gmail.com>
2019-12-03 18:08:06 +0100

    Fix 'Reviews' & 'AverageRating' for normalized data

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {Row, Col, Typography, Rate, Card} from 'antd'
 import styles from './review.module.css'
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
 
-const Review = ({review}) => (
+const Review = ({review, user}) => (
   <Card className={styles.review}>
     <Row type="flex" align="middle">
       <Col xs={24} md={16} align="left">
         <Typography.Title className={styles.name} level={4}>
-          {review.user}
+          {user.name}
         </Typography.Title>
         <Typography.Text className={styles.comment}>
           {review.text}
@@ -22,7 +23,7 @@ const Review = ({review}) => (
 )
 
 export const ReviewPropTypes = {
-  user: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
 }
@@ -31,4 +32,13 @@ Review.propTypes = {
   review: PropTypes.shape(ReviewPropTypes),
 }
 
-export default Review
+const mapStateToProps = (state, ownProps) => {
+  const review = state.reviews[ownProps.id]
+  const user = state.users[review.userId]
+  return {
+    review,
+    user,
+  }
+}
+
+export default connect(mapStateToProps)(Review)
--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -8,14 +8,15 @@ class Reviews extends Component {
   static defaultProps = {
     reviews: [],
   }
+
   render() {
     const {reviews, id} = this.props
+
     return (
       <Row type="flex" justify="center" gutter={{xs: 8, sm: 16, md: 24}}>
         <Col xs={24} md={16}>
           {reviews.map(reviewId => (
-            // <Review id={reviewId} key={reviewId} />
-            <div key={reviewId} />
+            <Review id={reviewId} key={reviewId} />
           ))}
           <ReviewForm id={id} />
         </Col>
